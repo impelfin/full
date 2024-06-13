5869ef62f361191a91a6f329f53f5d0337187832
impelfin <impelfin@gmail.com>
2024-06-13 01:52:47 +0000

    p

---
+++ ai-1/1_openai/6_speech_to_text/p162_app.py
@@ -0,0 +1,8 @@
+import openai
+
+audio_file = open("audio.mp3", "rb")
+
+transcript = openai.Audio.translate("whisper-1", audio_file)
+
+print(transcript["text"])
+
---
+++ ai-1/1_openai/7_tokenizer/p165_app.py
@@ -0,0 +1,11 @@
+import tiktoken
+
+enc = tiktoken.get_encoding("cl100k_base")
+
+tokens = enc.encode("hello world")
+print(len(tokens))
+print(tokens)
+
+print(enc.decode(tokens))
+
+print(enc.decode_tokens_bytes(tokens))
